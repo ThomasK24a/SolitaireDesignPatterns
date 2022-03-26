cd72fdab6b912f3c47afecb2ce4b1f12fc6da3cb
thomas koops <thomas_koops@hotmail.com>
2022-03-26 14:32:46 +0100

    Add board and board factory,

--- src/main/java/com/nhlstenden/solitaire/Classes/FinishStack.java
+++ src/main/java/com/nhlstenden/solitaire/Classes/FinishStack.java
@@ -1,6 +1,12 @@
 package main.java.com.nhlstenden.solitaire.Classes;
 
 import main.java.com.nhlstenden.solitaire.Abstract.CardStack;
+import main.java.com.nhlstenden.solitaire.Enums.Suit;
 
 public class FinishStack extends CardStack {
+    private Suit suit;
+
+    public FinishStack(Suit suit) {
+
+    }
 }
---
+++ src/main/java/com/nhlstenden/solitaire/Classes/GameBoard.java
@@ -0,0 +1,42 @@
+package main.java.com.nhlstenden.solitaire.Classes;
+
+import java.util.ArrayList;
+import main.java.com.nhlstenden.solitaire.Classes.Factory.BoardFactory;
+import main.java.com.nhlstenden.solitaire.Enums.Suit;
+
+public class GameBoard {
+    private final int BOARD_STACKS_AMOUNT = 7;
+
+    private ArrayList<BoardStack> boardStacks;
+    private ArrayList<FinishStack> finishStacks;
+    private DeckStack deck;
+    private WasteStack waste;
+    private BoardFactory boardFactory;
+
+    public GameBoard() {
+        boardStacks = createBoardStacks();
+        finishStacks = createFinishStacks();
+        boardFactory = new BoardFactory();
+        waste = new WasteStack();
+        deck = new DeckStack();
+
+        boardFactory.fillDeck(deck);
+        boardFactory.fillBoardStacks(boardStacks);
+    }
+
+    private ArrayList<BoardStack> createBoardStacks(){
+        ArrayList<BoardStack> boardStacks = new ArrayList<>();
+        for(int i = 0; i < BOARD_STACKS_AMOUNT; i++){
+            boardStacks.add(new BoardStack());
+        }
+        return boardStacks;
+    }
+
+    private ArrayList<FinishStack> createFinishStacks(){
+        ArrayList<FinishStack> finishStacks = new ArrayList<>();
+        for(Suit suit : Suit.class.getEnumConstants()){
+            finishStacks.add(new FinishStack(suit));
+        }
+        return finishStacks;
+    }
+}
