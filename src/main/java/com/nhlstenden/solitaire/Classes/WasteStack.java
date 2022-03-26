9f6dee92abc63f8452721082ed41cf2abe64f6ef
thomas koops <thomas_koops@hotmail.com>
2022-03-26 15:23:42 +0100

    Add can accept stack and is intractable logic to card stack

--- src/main/java/com/nhlstenden/solitaire/Classes/WasteStack.java
+++ src/main/java/com/nhlstenden/solitaire/Classes/WasteStack.java
@@ -1,6 +1,18 @@
 package main.java.com.nhlstenden.solitaire.Classes;
 
 import main.java.com.nhlstenden.solitaire.Abstract.CardStack;
+import main.java.com.nhlstenden.solitaire.Interfaces.ICard;
+
+import java.util.ArrayList;
 
 public class WasteStack extends CardStack {
+    @Override
+    public boolean canAcceptStack(ArrayList<ICard> cardStack) {
+        return false;
+    }
+
+    @Override
+    public boolean isIntractable(int cardIndex) {
+        return false;
+    }
 }
