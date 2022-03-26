d9e348cf0e2eb0c5551fb81e73e6e80a61fd1e30
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2022-03-26 12:35:18 +0100

    trying instatiation of cards

--- src/main/java/com/nhlstenden/solitaire/Abstract/CardStack.java
+++ src/main/java/com/nhlstenden/solitaire/Abstract/CardStack.java
@@ -1,4 +1,18 @@
 package main.java.com.nhlstenden.solitaire.Abstract;
 
+import main.java.com.nhlstenden.solitaire.Classes.Card;
+
+import java.util.ArrayList;
+import java.util.*;
+
 public abstract class CardStack {
+    private ArrayList<Card> cards;
+
+    public CardStack() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCards(ArrayList<Card> cards) {
+        this.cards.addAll(cards);
+    }
 }
--- src/main/java/com/nhlstenden/solitaire/GameManager.java
+++ src/main/java/com/nhlstenden/solitaire/GameManager.java
@@ -5,6 +5,5 @@ import main.java.com.nhlstenden.solitaire.GUI.MainMenu;
 public class GameManager {
     public static void main(String[] args) {
         MainMenu menu = new MainMenu();
-//        GameBoard gameBoard = new GameBoard();
     }
 }
