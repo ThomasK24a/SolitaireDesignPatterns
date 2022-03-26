1f04fae5386da6b7398429ab8a156a25de217125
thomas koops <thomas_koops@hotmail.com>
2022-03-26 15:23:24 +0100

    Add is black and is next value methods to card

--- src/main/java/com/nhlstenden/solitaire/Classes/Card.java
+++ src/main/java/com/nhlstenden/solitaire/Classes/Card.java
@@ -6,7 +6,7 @@ import main.java.com.nhlstenden.solitaire.Interfaces.ICard;
 
 import javax.swing.*;
 
-public class Card extends JComponent implements ICard {
+public class Card extends JPanel implements ICard {
     private final Suit suit;
     private final Value value;
     private boolean isFaceUp;
@@ -28,5 +28,23 @@ public class Card extends JComponent implements ICard {
 
     public boolean isFaceUp() {
         return isFaceUp;
+    }
+
+    public boolean isBlack() {
+        return suit.equals(Suit.CLUB) || suit.equals(Suit.SPADE);
+    }
+
+    /**
+     * Check if the given value is the next value after this card's value,
+     * if this card is an ace the next value will be a two, three is after two, etc.
+     * Jack is after ten, queen after jack, king and queen and after king there is no next value
+     * @param value value of the card to check
+     * @return return true if the given value is the next value
+     */
+    @Override
+    public boolean isNextValue(Value value) {
+        int intValue = getValue().ordinal();
+        int intValueToCompare = value.ordinal();
+        return intValue == intValueToCompare + 1;
     }
 }
