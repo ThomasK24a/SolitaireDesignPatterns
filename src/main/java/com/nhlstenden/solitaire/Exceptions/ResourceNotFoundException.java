f8225073b55710032c5143a0929cc51af3a30dcf
ThomasK24a <Thomas_koops@hotmail.com>
2022-03-27 17:09:02 +0200

    Add ResourceNotFoundException

---
+++ src/main/java/com/nhlstenden/solitaire/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package main.java.com.nhlstenden.solitaire.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resourceName) {
+        super(String.format("Could not find resource %s", resourceName));
+    }
+}
